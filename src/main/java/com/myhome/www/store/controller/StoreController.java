7f55d142a2945d8bf34527ba3cd82db2666ec085
seohee03 <60213901+seohee03@users.noreply.github.com>
2020-05-30 11:14:08 +0900

    store page 진입 추가

--- src/main/java/com/myhome/www/store/controller/StoreController.java
+++ src/main/java/com/myhome/www/store/controller/StoreController.java
@@ -1,6 +1,32 @@
 package com.myhome.www.store.controller;
 
-public class StoreController {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.myhome.www.item.service.ItemCommand;
+import com.myhome.www.item.service.ItemService;
+
+@Controller
+public class StoreController {
+	
+	@Resource(name = "itemService")
+	private ItemService itemService;
+
+	
+	//상품 리스트
+	 @RequestMapping(value = "/store") 
+	 public String itemListForMember(Model model) throws Exception { 
+		 
+		 List<ItemCommand> itemList = itemService.selectItemList();
+		 
+		 model.addAttribute("itemList", itemList);
+		
+		 return "store/storeHome"; 
+	 }
 	
 }
