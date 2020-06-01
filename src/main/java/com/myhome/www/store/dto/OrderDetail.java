fdf7bffead57bc1f0a037a3e6cd803a96d4f24a0
seohee03 <60213901+seohee03@users.noreply.github.com>
2020-06-01 14:22:23 +0900

    order dto 작성

---
+++ src/main/java/com/myhome/www/store/dto/OrderDetail.java
@@ -0,0 +1,72 @@
+package com.myhome.www.store.dto;
+
+import java.time.LocalDateTime;
+
+public class OrderDetail {
+
+	private int orderNo;				// 주문 번호
+	private int memberNo;				// 회원 번호
+	private String memberName;			// 회원 이름
+	private String memberTel;			// 회원 전화번호
+    private LocalDateTime orderDate;	// 주문 날짜
+	private int postcode;				// 우편번호
+    private String address;				// 주소
+    private String detail_addr;			// 상세주소
+    private String order_message;		// 배송 메모
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberTel() {
+		return memberTel;
+	}
+	public void setMemberTel(String memberTel) {
+		this.memberTel = memberTel;
+	}
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+	public int getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(int postcode) {
+		this.postcode = postcode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDetail_addr() {
+		return detail_addr;
+	}
+	public void setDetail_addr(String detail_addr) {
+		this.detail_addr = detail_addr;
+	}
+	public String getOrder_message() {
+		return order_message;
+	}
+	public void setOrder_message(String order_message) {
+		this.order_message = order_message;
+	}
+    
+    
+}
