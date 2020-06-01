994765fb29b58ce8126bb58e9fd4182d35f6e87b
seohee03 <60213901+seohee03@users.noreply.github.com>
2020-06-01 09:50:35 +0900

    pom 수정

--- src/main/java/com/myhome/www/store/controller/CartController.java
+++ src/main/java/com/myhome/www/store/controller/CartController.java
@@ -52,14 +52,11 @@ public class CartController {
 				result = cartService.updateItemInCart(cart);
 			}
 			
-			
 			if(result > 0) {
 				 writer.println("<script>alert('장바구니 추가 성공!'); location.href='/www/itemDetail/"+cart.getItemNo()+"';</script>");
 			}
-//			return "redirect:/";
 		}else {
 			writer.println("<script>alert('로그인을 먼저 해주세요!'); location.href='/www/login';</script>");
-			//return "login/loginForm";
 		}
 	}
 	
@@ -70,13 +67,11 @@ public class CartController {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter writer = response.getWriter();
 		
-		
 		if(authInfo != null) {
 			//로그인 한거면
 			writer.println("<script>location.href='/www/mycart?type=0';</script>");
 		} else {
 			writer.println("<script>alert('로그인을 먼저 해주세요!'); location.href='/www/login';</script>");
-			//return "login/loginForm";
 		}
 	}
 	
@@ -104,7 +99,6 @@ public class CartController {
 		System.out.println(">>>>>>>>>>>>>>"+cartNo);
 		System.out.println(">>>>>>>>>>>>>>"+newAmount);
 		int result = 0;
-		//int cartNo = cartCommand.getCartNo();
 
 		//카트 수량 업데이트 해주기
 		int res = cartService.updateItemAmountInCart(cartNo, newAmount);
@@ -116,45 +110,7 @@ public class CartController {
 		return result;
 	}
 	
-	
-	
-//	//장바구니에서 수량변경 버튼 누르면
-//	@ResponseBody
-//	@RequestMapping(value = "/updateCart")
-//	public int updateCartItemAmount(CartCommand cartCommand) {
-//		System.out.println("cartCommand.getAmount()"+cartCommand.getAmount());
-//		
-//		int result = 0;
-//		//카트 수량 업데이트 해주기
-//		int res = cartService.updateItemAmountInCart(cartCommand);
-//		if(res > 0) {
-//			result = 0;
-//		}else {
-//			result = 9;
-//		}
-//		return result;
-//	}
-	
-	
-//	@RequestMapping(value="/cart/update.do")
-//	public ModelAndView updateCart(Cart cart, HttpServletRequest request){
-//		ModelAndView mav = new ModelAndView("redirect:/cart/list.do");
-//		HttpSession loginSession = request.getSession();
-//		cart.setUser_id((String)loginSession.getAttribute("user_id"));
-//		int chk = cartService.searchCnt(cart.getProduct_id());
-//		System.out.println(chk);
-//		if(chk < cart.getCart_cnt()){
-//			mav.addObject("update_msg", "fail");
-//			mav.addObject("max_cnt", chk);
-//			return mav;
-//		}else{
-//			cartService.updateCart(cart);
-//			mav.addObject("update_msg", "success");
-//			return mav;
-//		}
-//		
-//	}
-	
+
 	
 	
 }
